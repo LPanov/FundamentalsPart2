2cac5f7fed1fa48e7b372f10c6dde8d625cedec4
LPanov <lubomirpanov99@gmail.com>
2024-08-09 18:17:31 +0300

    Added Bitwise Operations lab

--- src/BitwiseOperations.java
+++ src/BitwiseOperations.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BitwiseOperations {
@@ -5,12 +6,17 @@ public class BitwiseOperations {
 
     public void binaryDigitCount() {
         int n = Integer.parseInt(sc.nextLine());
-        char b = sc.nextLine().charAt(0);
+        byte digit = Byte.parseByte(sc.nextLine());
+
         int count = 0;
 
-        String binary = Integer.toBinaryString(n);
-        for (char c : binary.toCharArray()) {
-            if (c == b) count++;
+        while (n != 0) {
+
+            int leftover = n % 2;
+            if (leftover == digit) {
+                count++;
+            }
+            n = n / 2;
         }
 
         System.out.println(count);
@@ -18,12 +24,53 @@ public class BitwiseOperations {
 
     public void bitPosition() {
         int n = Integer.parseInt(sc.nextLine());
-        String b = Integer.toBinaryString(n);
-
-        System.out.println(b.charAt(b.length()-2));
+        int mask = 1 << 1;
+        int result = (n & mask) >> 1;
+        System.out.println(result);
     }
+
+    public void pThBit() {
+        int n = Integer.parseInt(sc.nextLine());
+        int p = Integer.parseInt(sc.nextLine());
+
+        int mask = 1 << p;
+        int result = (n & mask) >> p;
+
+        System.out.println(result);
+    }
+
+    public void bitDestroyer() {
+        int n = Integer.parseInt(sc.nextLine());
+        int p = Integer.parseInt(sc.nextLine());
+
+        int mask = ~(1 << p);
+        int result = (n & mask);
+
+        System.out.println(result);
+    }
+
+    public void oddTimes() {
+        int[] numbers = Arrays.stream(sc.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        int result = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            result = result^numbers[i];
+        }
+
+        System.out.println(result);
+    }
+
+    public void triBitSwitch() {
+        int n = Integer.parseInt(sc.nextLine());
+        int p = Integer.parseInt(sc.nextLine());
+
+        int mask = 7 << p;
+        int result = n ^ mask;
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
         BitwiseOperations bo = new BitwiseOperations();
-        bo.bitPosition();
+        bo.triBitSwitch();
     }
 }
